42a8132c9e3179c967f6b14511536cb58eab19b4
Esteban Horn <estebanhxrn@gmail.com>
2024-03-12 19:11:02 -0300

    fix details

--- app/components/searcher.jsx
+++ app/components/searcher.jsx
@@ -17,15 +17,15 @@ export default function Searcher ({ champs }) {
 
   return (
     <div className='md:right-5 right-2 z-20 w-1/2 md:w-auto flex flex-col my-auto'>
-        <div className='flex justify-center items-center w-full'>
-          <input
-            name='Campeon'
-            className='bg-black bg-opacity-70 rounded-xl text-sm text-white px-3 p-1 w-full placeholder:text-gray-200 outline-none border-2 border-white focus:border-2 focus:border-gray-300 focus:bg-black focus:bg-opacity-80 transition-all duration-300'
-            onChange={(e) => setInput(e?.target?.value)}
-            placeholder='Search'
-          />
-          <AiOutlineSearch className='relative right-6 text-white' />
-        </div>
+      <div className='flex justify-center items-center w-full'>
+        <input
+          name='Campeon'
+          className='bg-black bg-opacity-70 rounded-xl text-sm text-white px-3 p-1 w-full placeholder:text-gray-200 outline-none border-2 border-white focus:border-2 focus:border-gray-300 focus:bg-black focus:bg-opacity-80 transition-all duration-300'
+          onChange={(e) => setInput(e?.target?.value)}
+          placeholder='Search'
+        />
+        <AiOutlineSearch className='relative right-6 text-white' />
+      </div>
 
       {Input.length > 0 && (
         <ul className='bg-black w-full rounded-xl py-2 mt-1 bg-opacity-60 overflow-x-hidden '>
